2f813e3b2966af6c96eb25dad352deec966aa0a1
Graham Bevan <graham.bevan@ntlworld.com>
2024-08-27 18:49:21 +0100

    added to_Y for S

--- ycx_complex_numbers/S.py
+++ ycx_complex_numbers/S.py
@@ -9,6 +9,7 @@ class S(Complex):
     def __init__(self, c=None):
         super().__init__(c)
 
+
 class NetS(Net):
     def __init__(self, s11=None, s12=None, s21=None, s22=None):
         super().__init__(c11=s11, c12=s12, c21=s21, c22=s22)
@@ -16,12 +17,31 @@ class NetS(Net):
     @property
     def s11(self):
         return self._c11
+
     @property
     def s12(self):
         return self._c12
+
     @property
     def s21(self):
         return self._c21
+
     @property
     def s22(self):
         return self._c22
+
+    def to_Y(self, Z0=50 + 0j):
+        return NetY(
+            y11=((1 + self.s22) * (1 - self.s11) + self.s12 * self.s21)
+            / ((1 + self.s11) * (1 + self.s22) + self.s12 * self.s21)
+            * (1 / Z0),
+            y12=(-2 * self.s12)
+            / ((1 + self.s11) * (1 + self.s22) - self.s12 * self.s21)
+            * (1 / Z0),
+            y21=(-2 * self.s21)
+            / ((1 + self.s11) * (1 + self.s22) - self.s12 * self.s21)
+            * (1 / Z0),
+            y22=((1 + self.s11) * (1 - self.s22) + self.s12 * self.s21)
+            / ((1 + self.s22) * (1 + self.s11) - self.s12 * self.s21)
+            * (1 / Z0),
+        )
