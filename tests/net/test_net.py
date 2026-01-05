a6eefa1cdcba039f42b8ef05f794988ccc102a15
Graham Bevan <graham.bevan@ntlworld.com>
2026-01-05 16:27:38 +0000

    cleanup

--- tests/net/test_net.py
+++ tests/net/test_net.py
@@ -288,8 +288,8 @@ class TestNetConversions:
         assert zz.to_H().equals(h, precision=12)
         ss = h.to_S()
         assert isinstance(ss, NetS)
-        print(f"h={h}")
-        print(f"ss->h={ss.to_H()}")
+        # print(f"h={h}")
+        # print(f"ss->h={ss.to_H()}")
         assert ss.to_H().equals(h, precision=12)
 
         s = s1
